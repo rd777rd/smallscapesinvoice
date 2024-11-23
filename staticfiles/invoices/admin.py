e961dcd4ab3d940a0f41f77639945c060f2bc61a
Roman Drake <roman.drake.7@gmail.com>
2024-11-23 17:04:39 -0500

    first commit

--- staticfiles/invoices/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Invoice, InvoiceItem,Supply
-
-# Register your models here.
-admin.site.register(Invoice)
-admin.site.register(InvoiceItem)
-admin.site.register(Supply)
