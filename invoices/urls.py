ce94efa716af8d6edaefdc7a34e38478d0baf74d
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-11-23 21:45:29 -0500

    Update urls.py

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,7 +1,6 @@
-# urls.py
-from django.urls import path
+from django.urls import path,include
 from django.contrib.auth import views as auth_views
-from .views import InvoiceListView, CreateInvoiceView, CreateSupplyView, CreateInvoiceItemView, RemoveSupplyView,custom_logout_view, signup
+from .views import InvoiceListView, CreateInvoiceView, CreateSupplyView, CreateInvoiceItemView,custom_logout_view, signup,custom_404_view
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
@@ -11,5 +10,5 @@ urlpatterns = [
     path('create-invoice/', CreateInvoiceView.as_view(), name='create_invoice'),
     path('create-supply/', CreateSupplyView.as_view(), name='create_supply'),
     path('create-invoice-item/', CreateInvoiceItemView.as_view(), name='create_invoice_item'),
-    path('remove-supply/<int:invoice_id>/<int:item_id>/<int:supply_id>/', RemoveSupplyView.as_view(), name='remove_supply'),
 ]
+
